a84ae1991bc59f34c5c3e7907cc986c9344bb26e
pavelDoldin <114822963+pavelDoldin@users.noreply.github.com>
2023-11-05 19:57:06 +0500

    add_Oop_Encapsulation_Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package oop;
-
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/oop/Program.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Program {
+    public static void main(String[] args) {
+        Oop oop = new Oop();
+        System.out.println(oop.decodingOop);
+
+        System.out.println("------------------------------------------------------------------------------");
+
+        Encapsulation encapsulation = new Encapsulation();
+        System.out.println(encapsulation.decodingEncapsulation);
+
+        System.out.println("-------------------------------------------------------------------------------");
+
+        Inheritance inheritance = new Inheritance();
+        System.out.println(inheritance.decodingInheritance);
+    }
+}
